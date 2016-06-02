89fb2ac3b97723275bd864ce77ce16450f371c7a
Bill Stiber <billstiber@gmail.com>
2016-06-01 21:11:57 -0400

    add method

--- lesson_21/method.rb
+++ lesson_21/method.rb
@@ -1,15 +1,15 @@
 # create a metod that doubles a number
-
 def double(input)
   number = input * 2
   return number
 end
-
   puts double(3)
 
+# Method simplified
+def double(num)
+     num * 2
+end
+  puts double(10)
 
-  def double(num)
-    return num * 2
-  end
-
-puts double(10)
+# a2 + b2 = c2 , c = √	a2 + b2
+# pythagorean_theorem
