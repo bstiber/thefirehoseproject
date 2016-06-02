63f73866eb565bdece0f7e71ae422c1a190d6d82
Bill Stiber <billstiber@gmail.com>
2016-06-02 00:32:49 -0400

    add new file

---
+++ lesson_24/ex2.rb
@@ -0,0 +1,18 @@
+# Revisit the exercise you wrote in the previous lesson. Ask the user if they like cats.
+# If they enter "yes", display "Ken does too."
+# Otherwise, display "Dogs are better!" (this time use the else keyword)
+
+puts " DO you like cats, answer (y/n):"
+answer = gets.chomp.downcase
+
+def like(answer)
+  if answer == "y"
+    return "Ken does too!"
+  elsif answer == "n"
+    return "dogs are better:"
+    else answer != "y" || answer != "n"
+      return "you must enter (y or n)"
+  end
+end
+
+puts like(answer)
