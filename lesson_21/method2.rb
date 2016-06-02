5d6d16a2edd09115df2680a407ba082960431051
Bill Stiber <billstiber@gmail.com>
2016-06-01 22:10:22 -0400

    add sqrt to file

--- lesson_21/method2.rb
+++ lesson_21/method2.rb
@@ -9,7 +9,6 @@ def multiply(a, b)
   a_doubled = a * 2
   b_doubled = b * 2
   a_multiplied_by_b = a_doubled * b_doubled
-  total = multiply + double
 end
 puts multiply(4, 4)
 puts total = multiply(4, 4) + double(4)
--- lesson_21/py_theorum.rb
+++ lesson_21/py_theorum.rb
@@ -1,11 +1,12 @@
 # a2 + b2 = c2 , c = √	a2 + b2
 # pythagorean_theorem
 
+
 def pythagorean_theorem(a, b)
   a_squared = a * a
   b_squared = b * b
   a_squared_plus_b_squared = a_squared + b_squared
+
+  return Math.sqrt(a_squared_plus_b_squared).to_i
 end
-
-
-puts pythagorean_theorem(3, 4)
+  puts pythagorean_theorem(3, 4)
