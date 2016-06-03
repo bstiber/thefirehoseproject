44f5f94f6cb17d841bc2988ca6befb14ad7bde8a
Bill Stiber <billstiber@gmail.com>
2016-06-02 21:00:58 -0400

    add final changes to array2.rb

--- lesson_29/array2.rb
+++ lesson_29/array2.rb
@@ -1,3 +1,15 @@
 
-women = ["jessica", "brandi", "jennifer"]
+women = ["jessica", "brandi", "jennifer", "------"]
 puts women
+women.push("minnie", "rebeka")
+
+
+women.each do |girls|
+  reversed_women = girls.reverse.upcase
+  puts reversed_women
+end
+
+women.each do |girls|
+  upcase_women = girls.upcase
+  puts upcase_women
+end
