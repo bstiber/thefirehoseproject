1448eae19b5ae93d68b1a93c08ef15e66ea2826d
Bill Stiber <billstiber@gmail.com>
2016-06-03 17:58:14 -0400

    add code changes and file num.rb

--- lesson_32/num.rb
+++ lesson_32/num.rb
@@ -1,7 +1,18 @@
+#anther food bar loop challenge practice session, mastering loops and conditionals.
 puts 'enter any number'
 num = gets.chomp.to_i
 
 num.times do |n|
-  n += 1
-  puts n
+    n = n + 1 # add one to n so that the numbers start at 1 and not 0
+    a = n % 3 # number divisible by 3
+    b = n % 5 # number divisible by 5
+if  a == 0 && b == 0
+  puts "#{n}. is divisible by 3 and 5".upcase
+elsif a == 0
+  puts "#{n}. is divisible by 3"
+elsif b == 0
+  puts "#{n}. is divisible by 5"
+else
+  puts "#{n}."
+  end
 end
