1d0953af18bb4c29d8220c7e0af82a211a7921c1
Bill Stiber <billstiber@gmail.com>
2016-06-02 22:18:53 -0400

    final code add for ex1.rb

--- lesson_30/ex1.rb
+++ lesson_30/ex1.rb
@@ -1,1 +1,16 @@
 # Exercise sum numbers from 0 to 100
+
+# Build a new ruby script
+# Write a loop to get the sum of all numbers 0 through 100 (including 100)
+# At the end of the loop have it output "The sum is: sum" (Where sum is the actual number)
+
+count = 0
+sum = 0
+# count = sum = 0
+100.times do
+  count += 1
+  sum = sum + count
+  # sum += count
+puts "count equals #{count} plus sum #{sum}"
+puts "the sum is #{sum}"
+end
